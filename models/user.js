9bcaf2e355f8d26b8f77c8515a36858c6bacbd5a
tonysauvageau <toneeees@gmail.com>
2017-03-08 20:04:25 -0700

    updated auth

--- models/user.js
+++ models/user.js
@@ -4,4 +4,10 @@ let passportLocalMongoose - require('passport-local-mongoose');
 
 let User = new Schema({
   username: { type: String, unique: true, required: true, dropDups: true },
-})
+  password: { type: String },
+  role: { type: String, default: 'user' }
+});
+
+User.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', User);
